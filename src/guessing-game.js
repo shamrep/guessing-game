d5f34833945c3f79422dcef40fba933b130b7a11
shamrep <shamrep@gmail.com>
2023-10-28 23:34:05 +0300

    solve

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessValue = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessValue = Math.ceil((this.min + this.max) / 2);
+        return this.guessValue;
     }
 
     lower() {
-
+        this.max = this.guessValue;
     }
 
     greater() {
-
+        this.min = this.guessValue;
     }
 }
 
